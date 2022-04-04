83e4805e2bbce881c56ee4b2250122d5b076e852
Klaudia <35422977+Kaliop3@users.noreply.github.com>
2022-04-04 19:08:10 +0200

    adjust .gitignore

--- app/Program.cs
+++ app/Program.cs
@@ -1,28 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int i =0;
-//create array to sort
-int[] evenNums;
-evenNums= new int[5]{2, 1, 5, 3, 12};
 
-int n = evenNums.Length;
-int change;
+using System;
 
-while(n>1)
+namespace CSharpSort
 {
-    for(i=0; i<n-1; i++)
+    class Program
     {
-        if(evenNums[i]>evenNums[i+1])
+
+        // stdin: integers delimited by newline, finished with an empty line
+        // stdout: integers sorted, delimited by ", "
+
+        public static void Main(string[] args)
         {
-            change=evenNums[i+1];
-            evenNums[i+1]=evenNums[i];
-            evenNums[i]=change;
+            var list = new List<int>();
+
+            Console.WriteLine("Please write some number");
+            var getNumber = Console.ReadLine();
+
+
+            while(getNumber!=""){
+                int addNumber= int.Parse(getNumber);
+                list.Add(addNumber);
+                getNumber=Console.ReadLine();
+            }
+            list = Sort(list);
+            DisplayList(list);
+        }
+        public static void DisplayList(List<int> list)
+        {
+            foreach (int s in list)
+            {
+                Console.Write("\t{0}", s);
+            }
+        }
+        public static List<int> Sort(List<int> list)
+        {
+            int n = list.Count;
+            int change;
+
+            while (n > 1)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        change = list[i + 1];
+                        list[i + 1] = list[i];
+                        list[i] = change;
+                    }
+                }
+                n--;
+            }
+
+            return list;
         }
     }
-    n=n-1;
 }
-// print array sorted
-foreach (int s in evenNums)
-    {
-        Console.Write("\t{0}", s);   
-    }
 
+// https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/arrays-and-collections
+// https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.append?view=net-6.0#system-linq-enumerable-append-1(system-collections-generic-ienumerable((-0))-0)
+// https://docs.microsoft.com/en-us/dotnet/api/system.convert.toint32?view=net-6.0#system-convert-toint32(system-string)
